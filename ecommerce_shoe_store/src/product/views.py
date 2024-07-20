8ed2976869c550158dcf98a6e41e7baf82cdbdf4
Sahan Ratnayake <sahanr@calcey.com>
2024-07-20 13:24:19 +0530

    adds basic struct product view

--- ecommerce_shoe_store/src/product/views.py
+++ ecommerce_shoe_store/src/product/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import viewsets
 
-# Create your views here.
+class CollectionViewSet(viewsets.GenericViewSet):
+    def list(self, request, *args, **kwargs):
+        data = []
+        return HttpResponse(content=data)
+
+class ProductViewSet(viewsets.GenericViewSet):
+    def retrieve(self, request, *args, **kwargs):
+        data = {}
+        return HttpResponse(content=data)
+    def list(self, request, *args, **kwargs):
+        data = []
+        return HttpResponse(content=data)
